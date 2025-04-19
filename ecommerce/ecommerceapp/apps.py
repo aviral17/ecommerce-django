0d847902f73289ebe5eaa1d2925f5891e3a93028
Aviral <36530173+aviral17@users.noreply.github.com>
2025-04-19 22:34:04 +0530

    updated apps.py

--- ecommerce/ecommerceapp/apps.py
+++ ecommerce/ecommerceapp/apps.py
@@ -1,7 +1,7 @@
 # update it as per latest changes
 from django.apps import AppConfig
 
-
+# update it as per latest changes
 class EcommerceappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ecommerceapp'
